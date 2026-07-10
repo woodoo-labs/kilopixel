26e1d07b731b970677c6c0c822da9b99280fa68c
woodoo-labs <woodoo-labs@users.noreply.github.com>
2026-07-10 21:13:59 +0200

    Revert iOS ResizeObserver fix

--- js/elements/stage.js
+++ js/elements/stage.js
@@ -44,7 +44,7 @@ class Stage extends HTMLElement {
     this.style.aspectRatio ||= '16 / 9';
 
     this.resizeObserver = new ResizeObserver(() => this.resize());
-    this.resizeObserver.observe(this);
+    this.resizeObserver.observe(this, { box: 'device-pixel-content-box' });
 
     this.addEventListener('pointermove', this.interaction);
     this.addEventListener('pointerdown', this.interaction);
